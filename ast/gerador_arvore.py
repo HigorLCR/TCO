e2c67ad77a1c8d2095f8ab5778b56700cafbf816
higor.4446 <higor.4446@gmail.com>
2025-09-14 23:51:49 -0300

    feat: :sparkles: Refatora estrutura do gerador de árvore profundamente
    - O gerador de árvore agora teve suas funções refatoradas para coletar mais consistentemente informações como, bloco de recursão, bloco de condição de parada, condição de parada, argumentos da função e argumentos passados na recursão, de modo a fazer a construção iterativa mais robusta

--- ast/gerador_arvore.py
+++ ast/gerador_arvore.py
@@ -1,114 +1,6 @@
 import ast
 import argparse
 
-def converter_condicional_recursao_cauda(stmt, func):
-    if isinstance(stmt, ast.If):
-        # Extrai a condição do if
-        condition = find_stop_condition(stmt)
-        
-        # Extrai o corpo do if (bloco verdadeiro)
-        stop_condition_block = find_stop_condition_return(stmt)
-        
-        # Extrai o corpo do else (bloco falso), se existir
-        false_block = stmt.orelse if stmt.orelse else []
-        last_stmt = false_block[-1]
-        if (
-            isinstance(last_stmt, ast.Return) and 
-            isinstance(last_stmt.value, ast.Call) and 
-            isinstance(last_stmt.value.func, ast.Name) and
-            last_stmt.value.func.id == func.name
-        ):
-            while_loop = ast.While(
-                test=ast.UnaryOp(
-                    op = ast.Not(),
-                    operand=condition
-                ),
-                body=[
-                    false_block[:-1], 
-                    ast.Assign(
-                        targets=[ast.Tuple(
-                            elts=[
-                                ast.Name(id=func.args.args[0].arg, ctx=ast.Store()),
-                                ast.Name(id=func.args.args[1].arg, ctx=ast.Store())
-                            ],
-                            ctx=ast.Store()
-                        )],
-                        value=ast.Tuple(
-                            elts=[
-                                false_block[-1].value.args[0],
-                                false_block[-1].value.args[1]
-                            ],
-                            ctx=ast.Load()
-                        )
-                    )
-                ],
-                orelse=[] #false_block
-            )
-
-            return ast.Module(
-                body=[while_loop, stop_condition_block],
-                type_ignores=[]
-            )
-    return stmt
-
-def is_tail_recursive(func_node, func_name=None):
-    """
-    Detecta se uma função é recursiva de cauda.
-    :param func_node: ast.FunctionDef, func_name: str
-    :return: bool
-    """
-    for stmt in ast.walk(func_node):
-        # Procura por um return no final do corpo da função
-        if isinstance(stmt, ast.Return):
-            # Verifica se o return é uma chamada à própria função
-            if isinstance(stmt.value, ast.Call) and isinstance(stmt.value.func, ast.Name):
-                if stmt.value.func.id == func_name:
-                    return True
-    return False
-
-#verificar consistência
-def is_recursive(func_node, func_name=None):
-    
-    """
-    Detecta se uma função é recursiva.
-    :param func_node: ast.FunctionDef, func_name: str
-    :return: bool
-    """
-    for stmt in ast.walk(func_node):
-        # Procura por um return no final do corpo da função
-        if isinstance(stmt, ast.Return):
-            for arg in ast.walk(stmt):
-                if isinstance(arg, ast.Call) and isinstance(arg.func, ast.Name):
-                    if arg.func.id == func_name:
-                        return True
-    return False
-
-#Fazer identificação pra garantir que é a condição de parada
-def find_stop_condition(node):
-    for stmt in ast.walk(node):
-        if isinstance(stmt, ast.If):
-            return stmt.test
-    return None
-
-#Fazer identificação pra garantir que é a condição de parada
-def find_stop_condition_return(node):
-    for stmt in ast.walk(node):
-        if isinstance(stmt, ast.Return):
-            if not (isinstance(stmt.value, ast.Call) and isinstance(stmt.value.func, ast.Name) and stmt.value.func.id == node.name):
-                return stmt.value
-    return None
-
-#checar
-def find_recursive_block(node, func_name):
-    for stmt in ast.walk(node):
-        if isinstance(stmt, ast.If):
-            for true_block in ast.walk(stmt):
-                #identifica o bloco com recursão
-                is_true_block_recursive = is_recursive(true_block, func_name)
-                if is_true_block_recursive:
-                    return true_block
-    return None
-
 class UpperCaseFunctionNames(ast.NodeTransformer):
     #adiciona "_tco" ao nome da função
     def visit_FunctionDef(self, node):
@@ -126,44 +18,189 @@ class UpperCaseFunctionNames(ast.NodeTransformer):
     def visit_Return(self, node):
         node.value = ast.Constant(value="teste")
         return node
+
+#Classe para visitar todos os nós da árvore de maneira completa
+class FullVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self.nodes = []
+
+    def print_type(self, node):
+        print("Visiting node:", type(node))
+        super().print_type(node)
+
+    def generic_visit(self, node):
+        self.nodes.append(node)
+        super().generic_visit(node)
+
+    def clear_nodes(self):
+        self.nodes = []
+
+    def get_nodes(self):
+        return self.nodes
+
+def convert_tail_recursive_to_loop(tree, func_name):
+    recursive_if = find_recursive_if_block(tree, func_name)
+
+    #arguments da função
+    signature_args = find_signature_args(tree, func_name)
+
+    #argumentos passados na recursão
+    recursion_args = find_recursion_args(recursive_if["recursive_block"], func_name)
+
+    # Extrai a condição do if
+    condition = recursive_if["stop_condition"]
+    
+    # Extrai o corpo do if (bloco verdadeiro)
+    stop_condition_block = recursive_if["stop_condition_block"]
+    
+    # Extrai o corpo do else (bloco falso), se existir
+    false_block = recursive_if["recursive_block"]
+
+    last_stmt = false_block[-1]
+    if (
+        isinstance(last_stmt, ast.Return) and 
+        isinstance(last_stmt.value, ast.Call) and 
+        isinstance(last_stmt.value.func, ast.Name) and
+        last_stmt.value.func.id == func_name
+    ):
+        while_loop = ast.While(
+            test=ast.UnaryOp(
+                op = ast.Not(),
+                operand=condition
+            ),
+            body=[
+                false_block[:-1], 
+                ast.Assign(
+                    targets=[ast.Tuple(
+                        elts=[
+                            ast.Name(id=signature_args.args[0].arg, ctx=ast.Store()),
+                            ast.Name(id=signature_args.args[1].arg, ctx=ast.Store())
+                        ],
+                        ctx=ast.Store()
+                    )],
+                    value=ast.Tuple(
+                        elts=[
+                            recursion_args[0],
+                            recursion_args[1]
+                        ],
+                        ctx=ast.Load()
+                    )
+                )
+            ],
+            orelse=[] #false_block
+        )
+        return ast.Module(
+            body=[while_loop, stop_condition_block],
+            type_ignores=[]
+        )
+    return tree
+
+def is_function_tail_recursive(func_node, func_name=None):
+    """
+    Detecta se uma função é recursiva de cauda.
+    :param func_node: ast.FunctionDef, func_name: str
+    :return: bool
+    """
+    for stmt in ast.walk(func_node):
+        # Procura por um return no final do corpo da função
+        if isinstance(stmt, ast.Return):
+            # Verifica se o return é uma chamada à própria função
+            if isinstance(stmt.value, ast.Call) and isinstance(stmt.value.func, ast.Name):
+                if stmt.value.func.id == func_name:
+                    return True
+    return False
+
+#verificar consistência
+def is_function_recursive(func_node, func_name=None):
+    """
+    Detecta se uma função é recursiva.
+    :param func_node: ast.FunctionDef, func_name: str
+    :return: bool
+    """
+    for stmt in ast.walk(func_node):
+        # Procura por um return no final do corpo da função
+        if isinstance(stmt, ast.Return):
+            for arg in ast.walk(stmt):
+                if isinstance(arg, ast.Call) and isinstance(arg.func, ast.Name):
+                    if arg.func.id == func_name:
+                        return True
+    return False
+
+def find_recursion_args(nodes, func_name):
+    for stmt in nodes:
+        if isinstance(stmt, ast.Return):
+            for arg in ast.walk(stmt):
+                if isinstance(arg, ast.Call) and isinstance(arg.func, ast.Name):
+                    if arg.func.id == func_name:
+                        return arg.args
+    return None
+
+def find_signature_args(nodes, func_name):
+    for stmt in nodes:
+        if isinstance(stmt, ast.FunctionDef) and stmt.name == func_name:
+            return stmt.args
+    return None
+
+def find_recursive_if_block(nodes, func_name):
+    for stmt in nodes:
+        if isinstance(stmt, ast.If):
+            is_true_block_recursive = False 
+            is_false_block_recursive = False
+
+            for node in stmt.body:
+                if is_function_recursive(node, func_name):
+                    is_true_block_recursive = True
+            for node in stmt.orelse:
+                if is_function_recursive(node, func_name):
+                    is_false_block_recursive = True
+            
+            if is_true_block_recursive:
+                return { 
+                    "recursive_block": stmt.body, 
+                    "stop_condition_block": stmt.orelse, 
+                    "stop_condition": stmt.test 
+                }
+            elif is_false_block_recursive:  
+                return { 
+                    "recursive_block": stmt.orelse, 
+                    "stop_condition_block": stmt.body, 
+                    "stop_condition": stmt.test 
+                }
+    return None
+
     
 parser = argparse.ArgumentParser()
 parser.add_argument('arquivo', help='Nome do arquivo de código fonte (dentro da pasta tail)')
+parser.add_argument('nome_funcao', help='Nome da função a ser analisada')
 parser.add_argument('-dump', action='store_true', help='Exibe detalhes da análise')
 parser.add_argument('-nt', action='store_true', help='Indica que a função não é de cauda')
 args = parser.parse_args()
-print(args)
 
 with open(f'../recursive_functions/{'non_tail' if args.nt else 'tail'}/{args.arquivo}') as file:
     source_code = file.read()
-    
     tree = ast.parse(source_code)
+    visitor = FullVisitor()
+
+    #coleta todos os nós da árvore e armazena em nodes
+    visitor.generic_visit(tree)
+    nodes = visitor.get_nodes()
+    visitor.clear_nodes()
+
+    func_name = args.nome_funcao
     
-    
-    isRecursive = is_recursive(tree, func_name=tree.body[0].name)
-    isTail = is_tail_recursive(tree, func_name=tree.body[0].name)
+    isRecursive = is_function_recursive(tree, func_name)
+    isTail = is_function_tail_recursive(tree, func_name)
+
+
+    new_code = convert_tail_recursive_to_loop(nodes, func_name)
+    ast.fix_missing_locations(new_code)
 
     if args.dump:
-        print(ast.unparse(tree))
-        print("É recursiva? \n", isRecursive, "\n")
-        print("É tail? \n", isTail, "\n")
-    
-        print("Condição de parada: \n", ast.unparse(find_stop_condition(tree)), "\n")
-        print("Corpo da condição de parada: \n", ast.unparse(find_stop_condition_return(tree)), "\n")
-    #print("Estrutura da árvore: \n", ast.dump(tree.body[0], indent=4), "\n")
+        print("Código original: \n", ast.unparse(tree))
 
+        print("\nNovo código: \n", ast.unparse(new_code))
 
-    #trabalhar nessa função
-    #condicao_parada=encontrar_condicao_parada(tree.body[0].body[0])
-
-
-    #print("STMT: ", ast.unparse(tree.body[0].body[0]))
-    #print("FUNC: ", ast.unparse(tree.body[0]))
-    #new_code = converter_condicional_recursao_cauda(tree.body[0].body[0], tree.body[0])
-    #ast.fix_missing_locations(new_code)
-    #print("\nNovo código: \n", ast.unparse(new_code))
-
-
-    #print("Árvore sintática: ", ast.dump(tree, indent=4)) #visualizar estrutura da árvore sintática
-    # print("Dados convertidos: ", ast.unparse(tree)) #visualizar código convertido
+        print("\nÁrvore sintática: \n", ast.dump(tree, indent=4)) 
+        
+        print("\nNova árvore sintática: \n", ast.dump(new_code, indent=4))
     
--- recursive_functions/tail/factorial.py
+++ recursive_functions/tail/factorial.py
@@ -1,5 +1,12 @@
 def tail_factorial(n, a):
     if n == 0:
+        c = True
+        if not c:
+            print("A: ", a)      
         return a
     else:
+        b=1
+        for i in range(1, n+1):
+            b *= i
+            print("B: ", b)
         return tail_factorial(n-1, n*a)
