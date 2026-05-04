c6229539bea24de07ad9acf4eea6bf95a6783eb8
higor.4446 <higor.4446@gmail.com>
2026-05-04 18:57:05 -0300

    refactor: 🔨 Ajusta a função tail_fibonacci para corrigir a lógica de retorno e parâmetros

--- recursive_functions/tail/fibonacci.py
+++ recursive_functions/tail/fibonacci.py
@@ -1,11 +1,10 @@
 import sys
 sys.setrecursionlimit(10000000)
 
-def tail_fibonacci(n, a=0, b=1):
-
-    if n == 0:
+def tail_fibonacci(n, a=1, b=1):
+    if n == 1:
         return a
-    elif n == 1:
+    elif n == 2:
         return b
     else:
         return tail_fibonacci(n - 1, b, a + b)
