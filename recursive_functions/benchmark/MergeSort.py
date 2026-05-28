de4ea6a05f93d55840b3fbca04c488555361498b
higor.rocha <higor.4446@gmail.com>
2026-05-27 23:20:02 -0300

    feat: ✨ Adiciona implementação da função MergeSort recursiva e não recursiva com medições de desempenho

---
+++ recursive_functions/benchmark/MergeSort.py
@@ -0,0 +1,29 @@
+
+def MergeSort_Rec(L, i, j):
+	#variáveis locais: L, i, j, m, 
+	def Merge(L, i, m, j):
+		Ln = []; p1 = i; p2 = m+1
+		for _ in range(j-i+1):
+			if p2 > j or (p1 <= m and L[p1] < L[p2]):
+				Ln.append(L[p1]); p1 += 1
+			else:
+				Ln.append(L[p2]); p2 += 1
+		for k in range(j-i+1):
+			L[i+k] = Ln[k]
+
+
+	if i < j:
+		m = (i+j)//2
+		MergeSort_Rec(L, i, m)
+		MergeSort_Rec(L, m+1, j)
+		Merge(L, i, m, j)
+
+
+
+import timeit
+
+n = 1_000
+qtd_execucoes = 1_000
+
+tempo = timeit.timeit(lambda: MergeSort_Rec(list(range(n, 0, -1)), 0, n - 1), number=qtd_execucoes)
+print(f"tempo médio de {qtd_execucoes}: {tempo:.4f}s total | {tempo/qtd_execucoes*1000:.4f}ms por chamada")
---
+++ recursive_functions/benchmark/MergeSort_nonrec.py
@@ -0,0 +1,52 @@
+def MergeSort_Rec(L, i, j):
+    _P = []
+    _P.append((L, i, j, None, None, None, 0))
+    while len(_P) > 0:
+        L, i, j, m, _r1, _r2, _s = _P.pop()
+        if _s == 0:
+            _r = None
+
+        def Merge(L, i, m, j):
+            Ln = []
+            p1 = i
+            p2 = m + 1
+            for _ in range(j - i + 1):
+                if p2 > j or (p1 <= m and L[p1] < L[p2]):
+                    Ln.append(L[p1])
+                    p1 += 1
+                else:
+                    Ln.append(L[p2])
+                    p2 += 1
+            for k in range(j - i + 1):
+                L[i + k] = Ln[k]
+        if _s in [1, 2] or (_s == 0 and i < j):
+            if _s == 0:
+                m = (i + j) // 2
+            if _s == 0:
+                _P.append((L, i, j, m, _r1, _r2, 1))
+                _P.append((L, i, m, None, None, None, 0))
+                _s = -1
+            elif _s == 1:
+                _r1 = _r
+                _s = 0
+            if _s == 0:
+                _r1
+            if _s == 0:
+                _P.append((L, i, j, m, _r1, _r2, 2))
+                _P.append((L, m + 1, j, None, None, None, 0))
+                _s = -1
+            elif _s == 2:
+                _r2 = _r
+                _s = 0
+            if _s == 0:
+                _r2
+            if _s == 0:
+                Merge(L, i, m, j)
+    return _r
+import timeit
+
+n = 1_000
+qtd_execucoes = 1_000
+
+tempo = timeit.timeit(lambda: MergeSort_Rec(list(range(n, 0, -1)), 0, n - 1), number=qtd_execucoes)
+print(f"tempo médio de {qtd_execucoes}: {tempo:.4f}s total | {tempo/qtd_execucoes*1000:.4f}ms por chamada")
